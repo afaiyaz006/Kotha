1e944401829d8f399908d53e6c12c0d60a889f15
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-11-07 00:36:46 +0600

    added chat assistant ai

---
+++ app/api/ai/chat/route.tsx
@@ -0,0 +1,53 @@
+import { CoreMessage, streamText } from 'ai';
+import { createGroq } from '@ai-sdk/groq';
+import { headers } from 'next/headers';
+
+// Simple in-memory store for rate limiting - only tracking count now
+const rateLimit = new Map<string, number>();
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const headersList = headers();
+  const ip = headersList.get('x-forwarded-for') || 'unknown';
+  
+  // Skip rate limiting if DEBUG is enabled
+  const isDebug = process.env.NEXT_PUBLIC_DEBUG === 'TRUE';
+  //const isDebug = false;
+  if (!isDebug) {
+    const currentCount = rateLimit.get(ip) || 0;
+    
+    if (currentCount >= 10) {
+      const stream = new ReadableStream({
+        start(controller) {
+          controller.enqueue('Maximum request limit reached for this IP address');
+          controller.close();
+        },
+      });
+      
+      return new Response(stream, {
+        status: 429,
+        headers: {
+          'Content-Type': 'text/plain; charset=utf-8',
+        },
+      });
+    }
+    
+    rateLimit.set(ip, currentCount + 1);
+  }
+
+  const { messages }: { messages: CoreMessage[] } = await req.json();
+  
+  const groq = createGroq({
+    baseURL: process.env.GROQ_API_URL || '',
+    apiKey: process.env.GROQ_API_KEY || ''
+  });
+  
+  const result = await streamText({
+    model: groq('mixtral-8x7b-32768'),
+    system: 'You are a helpful assistant that analyzes chat between two users.One is the user defined as `You` another is the other party suggest probable replies and be helpful.',
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,288 +1,286 @@
 "use client";
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from "react";
+import Markdown from 'react-markdown'
 import { io } from "socket.io-client";
-
-
+import { useChat } from "ai/react";
 export default function Chat() {
-
-  
-  const [messages, setMessages] = useState<any[]>([])
-  const [users, setUsers] = useState<any[]>([])
-  const socketRef = useRef<any>(null)
-  const [username, setUsername] = useState('anon')
-  const [selectUser, setSelectUser] = useState(String)
-  const [selectUserId,setSelectUserId] = useState(String)
-  const [is_connected,setIsConnected] = useState(false)
-  const [socketuserId,setSocketUserId] = useState('')
-  
-  
- 
-  const handleSelectUser = (userId:string,username:string) => {
-    if(userId){
-      setSelectUserId(userId)
-    }
-    if(username){
-      setSelectUser(username)
-    }
+  const [messages_between_users, setMessages] = useState<any[]>([]);
+  const [users, setUsers] = useState<any[]>([]);
+  const socketRef = useRef<any>(null);
+  const [username, setUsername] = useState("anon");
+  const [selectUser, setSelectUser] = useState(String);
+  const [selectUserId, setSelectUserId] = useState(String);
+  const [is_connected, setIsConnected] = useState(false);
+  const [socketuserId, setSocketUserId] = useState("");
+  const { messages, input, setInput, append } = useChat({
+    api: '/api/ai/chat',
   }
+  );
 
-  
+  const handleSelectUser = (userId: string, username: string) => {
+    if (userId) {
+      setSelectUserId(userId);
+    }
+    if (username) {
+      setSelectUser(username);
+    }
+  };
 
-
-  const allMessages = messages.map((message, index) => {
-    
-    if (message.from_userId==socketuserId){
+  const allMessages = messages_between_users.map((message, index) => {
+    if (message.from_userId == socketuserId) {
       return (
-        <div className="flex flex-col border-2 border-slate-950 my-1 p-1 ml-5 mx-1" key={index}>
+        <div
+          className="flex flex-col border-2 border-slate-950 my-1 p-1 ml-5 mx-1"
+          key={index}
+        >
           <strong>YOU</strong>
-          <div>
-            {message.content}
-          </div>
+          <div>{message.content}</div>
         </div>
-      )
-    }
-    else if(message.from_userId==selectUserId){
-      
+      );
+    } else if (message.from_userId == selectUserId) {
       return (
-        <div className="flex flex-col border-2 border-slate-950 my-1 p-1 mr-5 mx-1" key={index}>
+        <div
+          className="flex flex-col border-2 border-slate-950 my-1 p-1 mr-5 mx-1"
+          key={index}
+        >
           <strong>{message.from_username}</strong>
-          <div>
-            {message.content}
-          </div>
+          <div>{message.content}</div>
         </div>
-      )
+      );
     }
-    
-  })
+  });
   const countMessagesForUser = (userId: string) => {
-    return messages.filter((message) => message.from_userId === userId).length;
+    return messages_between_users.filter(
+      (message) => message.from_userId === userId
+    ).length;
   };
   const allUsers = users.map(([userId, userInfo]) => {
-    const counter=countMessagesForUser(userId)
-    
-  
+    const counter = countMessagesForUser(userId);
+
     if (userId != socketuserId) {
       return (
         <div className="flex flex-row " key={userId}>
-          <a href="#" onClick={()=>handleSelectUser(userId,userInfo.username)} className="rounded  border-2 border-slate-900 my-1 hover:bg-blue-400">
+          <a
+            href="#"
+            onClick={() => handleSelectUser(userId, userInfo.username)}
+            className="rounded  border-2 border-slate-900 my-1 hover:bg-blue-400"
+          >
             <div className="text-justify text-md align-text-bottom text-slate-900 p-2">
-              {userInfo.username}{(selectUserId==userId)?'':counter?"("+counter+")":''}
+              {userInfo.username}
+              {selectUserId == userId ? "" : counter ? "(" + counter + ")" : ""}
             </div>
           </a>
         </div>
-      )
-    }
-    else{
+      );
+    } else {
       return (
         <div className="flex flex-row " key={userId}>
-          <a href="#" className="rounded  border-2 border-slate-900 my-1 hover:bg-blue-400">
+          <a
+            href="#"
+            className="rounded  border-2 border-slate-900 my-1 hover:bg-blue-400"
+          >
             <div className="text-justify text-md align-text-bottom text-slate-900 p-2">
               {userInfo.username}(YOU)
             </div>
           </a>
         </div>
-      )
-
+      );
     }
-  })
-
-
+  });
 
   useEffect(() => {
     //console.log("HEADER: "+process.env.NEXT_PUBLIC_SECRET_HEADER!)
-    
-    var server_string=''
-    if (process.env.NEXT_PUBLIC_DEBUG=='True'){
-        server_string=process.env.NEXT_PUBLIC_CLIENT_SIDE_SERVER||''
-       
+
+    var server_string = "";
+    if (process.env.NEXT_PUBLIC_DEBUG == "True") {
+      server_string = process.env.NEXT_PUBLIC_CLIENT_SIDE_SERVER || "";
+    } else {
+      server_string = process.env.NEXT_PUBLIC_CLOUD_SERVER || "";
     }
-    else{
-        server_string=process.env.NEXT_PUBLIC_CLOUD_SERVER||''
-      
-    }
-    
-    const socket = io(server_string,
-      {
-        withCredentials: true,
-        extraHeaders: {
-          "my-custom-header": process.env.NEXT_PUBLIC_SECRET_HEADER! || ''
-        },
-      }
-    );
-    const usernameGet=localStorage.getItem('username')
-    if(usernameGet){
-      setUsername(usernameGet)
-    }
-    else {
+
+    const socket = io(server_string, {
+      withCredentials: true,
+      extraHeaders: {
+        "my-custom-header": process.env.NEXT_PUBLIC_SECRET_HEADER! || "",
+      },
+    });
+    const usernameGet = localStorage.getItem("username");
+    if (usernameGet) {
+      setUsername(usernameGet);
+    } else {
       alert("No username found! Redirecting to home.");
-      window.location.href = '/';
+      window.location.href = "/";
     }
-    socketRef.current = socket
-    
-    
-    
-    const sessionId=localStorage.getItem('sessionId')
-    
-    if (sessionId){
-      const userId=localStorage.getItem(sessionId.toString())
-     
-      socket.auth={
+    socketRef.current = socket;
+
+    const sessionId = localStorage.getItem("sessionId");
+
+    if (sessionId) {
+      const userId = localStorage.getItem(sessionId.toString());
+
+      socket.auth = {
         sessionId,
         userId,
         username,
-      }
-      
+      };
+
       //socket.auth={userId}
-     // socket.auth={username}
+      // socket.auth={username}
 
       ///console.log("USERID: ",userId,"SESSIONID: ",sessionId,"username: ",username)
-    }else{
-      socket.auth={username}
+    } else {
+      socket.auth = { username };
     }
-    
-    socket.on("connect",()=>{
-      console.log("Connected to server")
-      setIsConnected(true)
-        
-    })
-    socket.on("session",({sessionId,userId})=>{
-      socket.auth={
+
+    socket.on("connect", () => {
+      console.log("Connected to server");
+      setIsConnected(true);
+    });
+    socket.on("session", ({ sessionId, userId }) => {
+      socket.auth = {
         sessionId,
         userId,
         username,
-      }
-      localStorage.setItem("sessionId",sessionId)
-      localStorage.setItem(sessionId.toString(),userId.toString())
-      setSocketUserId(userId.toString())
-      //console.log("USERID: ",userId,"SESSIONID: ",sessionId,"username: ",username)  
+      };
+      localStorage.setItem("sessionId", sessionId);
+      localStorage.setItem(sessionId.toString(), userId.toString());
+      setSocketUserId(userId.toString());
+      //console.log("USERID: ",userId,"SESSIONID: ",sessionId,"username: ",username)
+    });
 
-    })
-    
-    socket.on("private_message",({content,from_username,from_userId,to,counter})=>{
-        setMessages(messages=>[...messages,{
-              content:content,
-              from_username:from_username,
-              from_userId:from_userId,
-              to:to,
-             
-            }
-          ]
-        )
-    })
-
-    
-
-    socket.on("all_users",(users)=>{
-      
-      setUsers(users)
-
-    })
-
-    
-
-    
-    return () => {
-      socket.disconnect()
-    }
-
-
-    },[username])
-
-    function sendMessage(event: any) {
-      event.preventDefault()
-      const message = event.target.elements.message_box.value
-    
-    
-      if(selectUserId){
-        socketRef.current.emit("private_message",
+    socket.on(
+      "private_message",
+      ({ content, from_username, from_userId, to, counter }) => {
+        setMessages((messages_between_users) => [
+          ...messages_between_users,
           {
-            content:message,
-            to:selectUserId
-          }
-        )
+            content: content,
+            from_username: from_username,
+            from_userId: from_userId,
+            to: to,
+          },
+        ]);
       }
-      if(selectUser){
-        setMessages(messages=>[...messages,{
-            content:message,
-            from_username:username,
-            from_userId:socketuserId,
-            to:selectUser,
-          
-        }])
+    );
 
-      }
-      
-      
+    socket.on("all_users", (users) => {
+      setUsers(users);
+    });
 
+    return () => {
+      socket.disconnect();
+    };
+  }, [username]);
+
+  function sendMessage(event: any) {
+    event.preventDefault();
+    const message = event.target.elements.message_box.value;
+
+    if (selectUserId) {
+      socketRef.current.emit("private_message", {
+        content: message,
+        to: selectUserId,
+      });
     }
-    
+    if (selectUser) {
+      setMessages((messages_between_users) => [
+        ...messages_between_users,
+        {
+          content: message,
+          from_username: username,
+          from_userId: socketuserId,
+          to: selectUser,
+        },
+      ]);
+    }
+  }
 
-    return (
-      <div className="container mx-auto">
-        <div className="mx-2 border-2 border-slate-950 m-1 p-1">
-          <div className="mx-2 content-center justify-center text-3xl">
-            Kotha-Chat
-          </div>
+  return (
+    <div className="container mx-auto">
+      <div className="mx-2 border-2 border-slate-950 m-1 p-1">
+        <div className="mx-2 content-center justify-center text-3xl">
+          Kotha-Chat
         </div>
-        <div className="mx-2 content-center justify-center rounded-xl align-middle shadow-lg">
-          <div className="flex flex-col-reverse space-x-2 rounded-xl shadow-lg sm:flex-row">
-
-            <div className="basis-1/4  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1 ">
-              <div className="p-3 scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300  h-64 overflow-y-scroll">
-               
-                <div className="flex">
-                  
-                  <div className="shadow-xl">
-                    <strong>
-                      Active users
-                    </strong>
-                    <br>
-                    </br>
-                    <span>Shows user currently active</span>
-                  </div>
+      </div>
+      <div className="mx-2 p-3 content-center justify-center rounded-xl align-middle shadow-lg">
+        <div className="flex flex-col-reverse space-x-2 rounded-xl shadow-lg sm:flex-row">
+          <div className="basis-1/4  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1 ">
+            <div className="p-3 scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300  h-64 overflow-y-scroll">
+              <div className="flex">
+                <div className="shadow-xl">
+                  <strong>Active users</strong>
+                  <br></br>
+                  <span>Shows user currently active</span>
                 </div>
-                <div className="flex flex-col font-bold">
-                  
-                  {allUsers}
-                </div>
-
               </div>
+              <div className="flex flex-col font-bold">{allUsers}</div>
             </div>
-            <div className="basis-1/2  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1">
-              <strong className="p-2 mx-auto">Connection Status: {is_connected?"Connected":"Connecting....."}</strong>
-              <p className='p-1'>Messages will appear here</p>
-              <p className="p-2">Click a active user to open chatbox. <strong>{selectUser}</strong></p>
-              <div className="scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300  h-64 overflow-y-scroll">
+          </div>
+          <div className="basis-1/2  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1">
+            <strong className="p-2 mx-auto">
+              Connection Status:{" "}
+              {is_connected ? "Connected" : "Connecting....."}
+            </strong>
+            <p className="p-1">messages_between_users will appear here</p>
+            <p className="p-2">
+              Click a active user to open chatbox. <strong>{selectUser}</strong>
+            </p>
+            <div className="scrollbar scrollbar-thumb-sky-700 scrollbar-track-sky-300  h-64 overflow-y-scroll">
               {allMessages}
-              </div>
-              <form onSubmit={sendMessage}>
-                <div className="flex flex-col">
-                  <strong className="m-1">Send Message:</strong>
-
-                  <textarea className="border-2 border-slate-700 focus:border-slate-950 p-1 m-1 row-5 focus:outline-none" id="message_box"></textarea>
-                  <button type="submit" className="border-2 border-slate-900 p-1 m-3 hover:bg-green-600">
-                    <p className="text-center">Send Message</p>
-                  </button>
-
-                </div>
-              </form>
-
             </div>
-            <div className="basis-1/2  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1">
-              <div className="flex flex-col border-2 border-slate-950 my-1 mx-1">
-                <strong className="p-2"> Intent</strong>
-                <div className="p-2">
-                  To be implemented.
-                </div>
-                <button className="border-2 border-slate-900 p-1 m-3 hover:bg-green-600">
-                  <p className="text-center">Analyze Intent</p>
+            <form onSubmit={sendMessage}>
+              <div className="flex flex-col">
+                <strong className="m-1">Send Message:</strong>
+
+                <textarea
+                  className="border-2 border-slate-700 focus:border-slate-950 p-1 m-1 row-5 focus:outline-none"
+                  id="message_box"
+                ></textarea>
+                <button
+                  type="submit"
+                  className="border-2 border-slate-900 p-1 m-3 hover:bg-green-600"
+                >
+                  <p className="text-center">Send Message</p>
                 </button>
               </div>
+            </form>
+          </div>
+          <div className="basis-1/2  hover:shadow-lg shadow-sm border-2 border-slate-950 my-1">
+            <div className="flex flex-col border-2 border-slate-950 my-1 mx-1">
+              <strong className="p-2">Chat Assistant</strong>
+              <p className="p-2">
+                {messages.map((message, index) => (
+                  <div key={index}>
+                    <Markdown>
+                    {message.content}
+                    </Markdown>
+                    </div>
+                ))}
+              </p>
+              <button className="border-2 border-slate-900 p-1 m-3 hover:bg-green-600" onClick={()=>{
+                let analysis_data=""
+              
+                for(const message of messages_between_users){
+                  //console.log(message)
+                  if (username==message.from_username){
+                    analysis_data+="user: "+"You"+"\n"
+                  }
+                  else{
+                    analysis_data+="user: "+message.from_username+"\n"
+                  }
+                  analysis_data+="message: "+message.content+"\n"
+                  analysis_data+="-"
+                  
+                }
+                setInput(analysis_data)
+                append({ content: input, role: 'user' });
+              }}>
+                <p className="text-center">Suggest Reply</p>
+              </button>
             </div>
           </div>
         </div>
       </div>
-
-
-    );
-  }
+    </div>
+  );
+}
